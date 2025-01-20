ee9d51661cbd0ba001d60940cbfeecf046c8410f
szuhow <magiedeszu@gmail.com>
2025-01-20 10:28:42 +0100

    chkpts

--- train.py
+++ train.py
@@ -31,7 +31,7 @@ def train_model(
         batch_size: int = 1,
         learning_rate: float = 1e-4,
         val_percent: float = 0.1,
-        save_checkpoint: bool = True,
+        save_checkpoint: bool = False,
         img_scale: float = 0.5,
         amp: bool = False,
         weight_decay: float = 1e-8,
